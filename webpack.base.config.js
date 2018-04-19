44055ce074d43662ac3449bf55501441af16bdbb
陈玉驰 <wb-chenyuchi@alibaba-inc.com>
2018-04-19 18:48:21 +0800

    fix

---
+++ webpack.base.config.js
@@ -0,0 +1,66 @@
+// webpack init
+const webpack = require('webpack');
+const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const env = process.env.WEBPACK_ENV;
+
+module.exports = {
+  entry: {
+    index: path.resolve(__dirname, 'src/index.js'),
+    vendors: ['react', 'react-dom', 'jquery']
+  },
+  output: {
+    filename: '[name].[chunkhash:6].js',
+    chunkFilename: '[name].[chunkhash].js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  mode: env,
+  performance: {
+    hints: false 	// 关闭warning日志信息
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        options: {
+          presets: ['env', 'react', 'es2017']
+        }
+      },
+      {
+        test: /\.(less|css)$/,
+        use: ExtractTextPlugin.extract({
+          use: ['css-loader', 'less-loader'],
+          fallback: 'style-loader'
+        })
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        loader: 'file-loader',
+        options: {
+          name: 'images/[name].[hash:8].[ext]'
+        }
+      },
+    ]
+  },
+  plugins: [
+    new ExtractTextPlugin('[name]-style.[hash:6].css'),
+    new HtmlWebpackPlugin({
+      title: 'webpack4 入门教程',
+      template: './index.html'
+    })
+  ],
+  optimization: {
+    splitChunks: {
+      chunks: 'initial',
+      cacheGroups: {
+        vendor: {
+          test: /node_modules\//,
+          name: 'vendors'
+        }
+      }
+    }
+  }
+};
--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -1,17 +1,11 @@
 const path = require('path');
 const baseConfig = require("./webpack.base.config");
-const CleanWebpackPlugin = require('clean-webpack-plugin');
 const merge = require("webpack-merge");
 const env = process.env.WEBPACK_ENV;
 console.log('environment: ', env);
 
 module.exports = merge(baseConfig, {
-	mode: env,
-	devServer: {
-		contentBase: path.resolve(__dirname)
-	},
-	devtool: 'source-map',	// map模式
-	plugins: [
-		new CleanWebpackPlugin('dist')
-	]
+  devServer: {
+    contentBase: path.resolve(__dirname)
+  }
 });
