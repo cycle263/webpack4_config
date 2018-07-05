17893e219ac8df05893c5f8117345939f88d5846
陈玉驰 <wb-chenyuchi@alibaba-inc.com>
2018-07-05 19:01:08 +0800

    整理blog - webpack4

---
+++ dist/buildFiles.md
@@ -0,0 +1,8 @@
+## Build files:
+
+- buildFiles.md
+- ./images/map.ce239.jpg
+- manifest.780e8.js
+- vendors~index.eacd2.js
+- index.79864.js
+- index-style.70704.css
--- webpack.base.config.js
+++ webpack.base.config.js
@@ -3,6 +3,7 @@ const webpack = require('webpack');
 const path = require('path');
 const pkg = require('./package.json'); // 引入package.json
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const BuildFilePlugin = require('build-file-plugin');
 const env = process.env.WEBPACK_ENV;
 
 module.exports = {
@@ -58,5 +59,6 @@ module.exports = {
       'VERSION': pkg.version
     }),
     new webpack.HashedModuleIdsPlugin(),  // 根据模块的相对路径生成一个四位数的hash作为模块id
+    new BuildFilePlugin(),
   ],
 };
